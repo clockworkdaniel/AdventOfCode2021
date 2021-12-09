dd9244fe3d2e1757141393091e2ea40e2526bf3f
clockworkdaniel <clockworkdaniel@gmail.com>
2021-12-09 09:12:13 +0000

    finish part 2

--- day8/day8-part2.test.ts
+++ day8/day8-part2.test.ts
@@ -1,14 +1,44 @@
-import { countInstancesOfLetter, getEntryValue, parseEntry } from "./day8-part2";
+import {
+  countInstancesOfLetter,
+  getEntryValue,
+  parseEntry,
+} from "./day8-part2";
 
-const exampleEntry = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf";
+const exampleEntry =
+  "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf";
 
 const parsedEntry = parseEntry(exampleEntry);
 
-
-test('countInstancesOfLetter', () => {
-  expect(countInstancesOfLetter(parsedEntry.signalPatterns)).toEqual({a: 8, b: 9, c: 7, d:8, e:6, f:7, g:4})
+test("countInstancesOfLetter", () => {
+  expect(countInstancesOfLetter(parsedEntry.signalPatterns)).toEqual({
+    a: 8,
+    b: 9,
+    c: 7,
+    d: 8,
+    e: 6,
+    f: 7,
+    g: 4,
+  });
 });
 
+const moreEntries = [
+  "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+  "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+  "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+  "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+  "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+  "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+  "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+  "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+  "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+  "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce",
+];
+
 test("getEntryValue", () => {
-  expect(getEntryValue(parsedEntry)).toBe("5353");
+  expect(getEntryValue(parsedEntry)).toBe(5353);
+
+  const parsedEntries = moreEntries.map(parseEntry);
+  expect(parsedEntries.map(getEntryValue)).toEqual([
+    8394, 9781, 1197, 9361, 4873, 8418, 4548, 1625, 8717, 4315,
+  ]);
 });
--- day8/day8-part2.ts
+++ day8/day8-part2.ts
@@ -12,9 +12,11 @@ export const parseEntry = (s: string): Entry => {
   return { signalPatterns, digitValues };
 };
 
-type LetterInstances = {[key: string]: number}
+type LetterInstances = { [key: string]: number };
 
-export const countInstancesOfLetter = (signalPatterns: string[]) : LetterInstances => {
+export const countInstancesOfLetter = (
+  signalPatterns: string[]
+): LetterInstances => {
   const letterInstances: LetterInstances = {
     a: 0,
     b: 0,
@@ -22,30 +24,30 @@ export const countInstancesOfLetter = (signalPatterns: string[]) : LetterInstanc
     d: 0,
     e: 0,
     f: 0,
-    g: 0
-  }
+    g: 0,
+  };
 
   Object.keys(letterInstances).forEach((letter: string) => {
-    const regex = new RegExp(`${letter}`, 'g')
+    const regex = new RegExp(`${letter}`, "g");
     const matches = signalPatterns.join().match(regex);
     const count = matches ? matches.length : 0;
     letterInstances[letter] = count;
   });
 
   return letterInstances;
-}
+};
 
-
-
-export const getEntryValue = ({signalPatterns, digitValues}: Entry) => {
-  const digitPatterns: string[] = new Array(10).fill('');
+export const getEntryValue = ({ signalPatterns, digitValues }: Entry) => {
+  const digitPatterns: string[] = new Array(10).fill("");
 
   const findByLengthAndRemoveFromSignalPatterns = (length: number): string => {
-    const i = signalPatterns.findIndex(sp => sp.length === length);
+    const i = signalPatterns.findIndex((sp) => sp.length === length);
     const letter = signalPatterns[i];
-    signalPatterns = signalPatterns.filter(sp => sp !== letter);
+    signalPatterns = signalPatterns.filter((sp) => sp !== letter);
     return letter;
-  }
+  };
+
+  const instancesOfLetter = countInstancesOfLetter(signalPatterns);
 
   // known
   digitPatterns[1] = findByLengthAndRemoveFromSignalPatterns(2);
@@ -53,56 +55,58 @@ export const getEntryValue = ({signalPatterns, digitValues}: Entry) => {
   digitPatterns[7] = findByLengthAndRemoveFromSignalPatterns(3);
   digitPatterns[8] = findByLengthAndRemoveFromSignalPatterns(7);
 
-  const instancesOfLetter = countInstancesOfLetter(signalPatterns);
+  let leftTop = "";
+  let leftBottom = "";
+  let rightBottom = "";
 
-  let leftTop = '';
-  let leftBottom = '';
-  let rightBottom = '';
-
-  
   Object.entries(instancesOfLetter).forEach(([letter, instances]) => {
     if (instances === 6) leftTop = letter;
     if (instances === 4) leftBottom = letter;
     if (instances === 9) rightBottom = letter;
-  })
+  });
 
-  const rightTop = digitPatterns[1].replace(rightBottom, '');
+  const rightTop = digitPatterns[1].replace(rightBottom, "");
 
-  const findBySegmenetAndRemoveFromSignalPatterns = (regexString: string, ofLength?: number): string => {
-    let letter: string;
+  const findBySegmenetAndRemoveFromSignalPatterns = (
+    regexString: string,
+    ofLength?: number
+  ): string => {
+    let letter = "";
     if (!ofLength) {
-      letter = signalPatterns.find(sp => new RegExp(regexString, 'g').test(sp) === false)!;
+      letter = signalPatterns.find(
+        (sp) => new RegExp(regexString).test(sp) === false
+      )!;
     } else {
-      letter = signalPatterns.filter(sp => sp.length === ofLength).find(sp => new RegExp(regexString, 'g').test(sp) === false)!;
+      letter = signalPatterns
+        .filter((sp) => sp.length === ofLength)
+        .find((sp) => new RegExp(regexString).test(sp) === false)!;
     }
-    signalPatterns = signalPatterns.filter(sp => sp !== letter);
-    // console.log(signalPatterns)
+    signalPatterns = signalPatterns.filter((sp) => sp !== letter);
     return letter;
-  }
+  };
 
-  console.log(leftBottom, leftTop)
+  digitPatterns[5] = findBySegmenetAndRemoveFromSignalPatterns(
+    `${leftBottom}|${rightTop}`
+  );
 
-  digitPatterns[5] = findBySegmenetAndRemoveFromSignalPatterns(`[${leftBottom}|${rightTop}]`);
   digitPatterns[6] = findBySegmenetAndRemoveFromSignalPatterns(rightTop, 6);
-  digitPatterns[9] = findBySegmenetAndRemoveFromSignalPatterns(leftBottom, 6); //not working
+  digitPatterns[9] = findBySegmenetAndRemoveFromSignalPatterns(leftBottom, 6); // not working
   digitPatterns[0] = findByLengthAndRemoveFromSignalPatterns(6);
-  digitPatterns[3] = findBySegmenetAndRemoveFromSignalPatterns(leftBottom); // not working
+  digitPatterns[3] = findBySegmenetAndRemoveFromSignalPatterns(leftBottom);
   digitPatterns[2] = signalPatterns[0];
 
-   console.log(digitPatterns);
-
-  return digitValues.reduce((output, dv) => {
-    const i = digitPatterns.findIndex(dp => dp === dv) // need to check for jumbled
-    return output + i
-  },'')
-
-
-
-
-
-
-
-
-}
+  return Number(
+    digitValues.reduce((output, dv) => {
+      const i = digitPatterns
+        .map((dp) => dp.split("").sort().join(""))
+        .findIndex((dp) => dp === dv.split("").sort().join(""));
+      return output + i;
+    }, "")
+  );
+};
 
 const parsedEntries = inputData.map((x) => parseEntry(x));
+
+const result = parsedEntries.map(getEntryValue).reduce((sum, e) => sum + e, 0);
+
+console.log(result);
