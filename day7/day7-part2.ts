d123cb1ad64d5d5788456ca7189f7d2eb6096343
clockworkdaniel <clockworkdaniel@gmail.com>
2021-12-14 00:34:57 +0000

    day7-part2

--- day7/day7-part2.ts
+++ day7/day7-part2.ts
@@ -1,14 +1,32 @@
 import inputData from "./inputData";
 
-export function getBestPosition(positions: number[]) {
+export const getBestPosition = (positions: number[]): number => {
   positions.sort((a, b) => a - b);
 
   let mid = Math.floor(positions.length / 2);
-  return positions[mid];
-}
+  const midVal = positions[mid];
 
-export const calcFuel = (desiredPosition: number, crabs: number[]) =>
-  crabs.reduce((fuel, c) => {
+  const direction =
+    calcFuel(midVal + 1, positions) < calcFuel(midVal - 1, positions) ? +1 : -1;
+
+  let i = midVal;
+
+  while (true) {
+    const test = calcFuel(i, positions);
+    const next = calcFuel(i + direction, positions);
+
+    if (next > test) {
+      break;
+    }
+
+    i++;
+  }
+
+  return i;
+};
+
+export const calcFuel = (desiredPosition: number, positions: number[]) =>
+  positions.reduce((fuel, c) => {
     const distance = Math.abs(c - desiredPosition);
     let cFuel = 0;
     let fuelCost = 1;
@@ -20,3 +38,5 @@ export const calcFuel = (desiredPosition: number, crabs: number[]) =>
 
     return fuel + cFuel;
   }, 0);
+
+console.log(calcFuel(getBestPosition(inputData), inputData));
--- day7/day7.ts
+++ day7/day7.ts
@@ -1,15 +1,15 @@
 import inputData from "./inputData";
 
-export function getBestPosition(positions: number[]) {
+export const getBestPosition = (positions: number[]) => {
   positions.sort((a, b) => a - b);
 
   let mid = Math.floor(positions.length / 2);
   return positions[mid];
-}
+};
 
 const bestPosition = getBestPosition(inputData);
 
-export const calcFuel = (desiredPosition: number, crabs: number[]) =>
-  crabs.reduce((fuel, c) => Math.abs(desiredPosition - c) + fuel, 0);
+export const calcFuel = (desiredPosition: number, positions: number[]) =>
+  positions.reduce((fuel, c) => Math.abs(desiredPosition - c) + fuel, 0);
 
 console.log(calcFuel(bestPosition, inputData));
